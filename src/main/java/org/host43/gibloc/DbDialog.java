8c3c510590add44ab1cf22280d027a94c9689972
stas <olovsv@gmail.com>
2016-12-27 16:35:49 +0300

    .

--- src/main/java/org/host43/gibloc/DbDialog.java
+++ src/main/java/org/host43/gibloc/DbDialog.java
@@ -1,6 +1,8 @@
 package org.host43.gibloc;
 
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,19 +21,37 @@ public class DbDialog {
 
   private DbDialog() throws ClassNotFoundException, SQLException {
     Class.forName("com.mysql.jdbc.Driver");
-    dbConn= DriverManager.getConnection("jdbc:mysql/jaba.gib.loc:3306/gibloc","hashez","hashezzz");
+    dbConn= DriverManager.getConnection("jdbc:mysql://jaba.gib.loc:3306/gibloc","admin","gibloc");
     stmt=dbConn.createStatement();
   }
 
-  public List<File> getClientFileset(String client) throws SQLException {
+  public List<File> getFileset(String client) throws SQLException, NoSuchAlgorithmException {
+    List<File> fileSet=new ArrayList<>();
+    int clientId=getClientId(client);
     ResultSet rs=stmt.executeQuery(
-        "select item,checksum,state from hashez_client where client=\""+client+"\""
-    );
+        "select item,checksum,state from hashez_file where client_id="+clientId);
+    String filename;
+    Checksum checksum;
+    State state;
+    byte[] digest;
     while(rs.next()){
-      String filename=rs.getNString("item");
-      byte[] checksum =rs.getBytes("checksum");
-      State state=State(4);
+      filename=rs.getString("item");
+      state=State.valueOf(rs.getString("state"));
+      digest=rs.getBytes("checksum");
+      if(digest!=null)
+        checksum=new Checksum(digest);
+      else
+        checksum=null;
+      fileSet.add(new File(filename,digest,state));
     }
-    return null;
+    return fileSet;
+  }
+
+  private int getClientId(String client) throws SQLException {
+    ResultSet rs=stmt.executeQuery("select id from hashez_client where item=\""+client+"\"");
+    int id=0;
+    if(rs.next())
+      id=rs.getInt("id");
+    return id;
   }
 }
--- src/main/java/org/host43/gibloc/State.java
+++ src/main/java/org/host43/gibloc/State.java
@@ -4,20 +4,10 @@ package org.host43.gibloc;
  * Created by stas on 19.12.2016.
  */
 public enum State {
-  OK(0),
-  UPDATED(1),
-  FILESYSTEMERROR(2),
-  CRYPTOERROR(3),
-  EMPTY(4)
+  OK,
+  UPDATED,
+  FILESYSTEMERROR,
+  CRYPTOERROR,
+  EMPTY
   ;
-
-  private final int state;
-
-  private State(int state){
-    this.state = state;
-  }
-
-  public int getState(){
-    return this.state;
-  }
 }
