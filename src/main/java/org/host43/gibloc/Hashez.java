4e1957ed9343c59ad3779699cbcdcd2f7c08539b
stas <olovsv@gmail.com>
2017-02-08 17:48:59 +0300

    .

--- src/main/java/org/host43/gibloc/Hashez.java
+++ src/main/java/org/host43/gibloc/Hashez.java
@@ -1,9 +1,8 @@
 package org.host43.gibloc;
 
-import javax.xml.bind.DatatypeConverter;
-import java.security.NoSuchAlgorithmException;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,16 +10,24 @@ import java.util.List;
  * Created by stas on 15.12.2016.
  */
 public class Hashez {
-  public static void main(String[] args) throws CommandLineException {
+  private static Logger log= LogManager.getLogger("Hashez");
+
+  public static void main(String[] args){
     UAction mode=null;
     List<String> argsArr=Arrays.asList(args);
     if(argsArr.size()<1){
       System.out.println("Error options");
     }else{
-      if(!argsArr.get(0).equals("gui")){
-        mode=new Console(argsArr);
-      }else{
-        mode=new Gui();
+      try{
+        if(!argsArr.get(0).equals("gui")){
+          mode=new Console(argsArr);
+        }else{
+          mode=new Gui();
+
+        }
+      }catch(CommandLineException e){
+        log.error(e);
+        System.exit(-1);
       }
     }
     assert mode != null;
